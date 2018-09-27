8456b118d857d4cebb9fcf1c204280bdc0106915
dmitrybugrov <dmitrybugrov@gmail.com>
2018-09-28 00:50:41 +0300

    update ExceptionController

--- src/main/java/ru/dmitrybugrov/salesDB/controllers/ExceptionController.java
+++ src/main/java/ru/dmitrybugrov/salesDB/controllers/ExceptionController.java
@@ -1,51 +1,64 @@
 package ru.dmitrybugrov.salesDB.controllers;
 
-import org.springframework.core.Ordered;
-import org.springframework.core.annotation.Order;
+
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
-import org.springframework.validation.BindException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.bind.MissingServletRequestParameterException;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 import org.springframework.web.servlet.NoHandlerFoundException;
-import org.springframework.web.servlet.mvc.method.annotation.ExceptionHandlerExceptionResolver;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 import ru.dmitrybugrov.salesDB.model.JsonError;
-import com.fasterxml.jackson.databind.exc.InvalidDefinitionException;
-
-import javax.persistence.EntityNotFoundException;
-
-import static org.springframework.http.HttpStatus.NOT_FOUND;
 
 
 @RestControllerAdvice
 public class ExceptionController extends ResponseEntityExceptionHandler {
 
-//
-//    @Override
-//    protected ResponseEntity<Object> handleHttpMessageNotReadable(HttpMessageNotReadableException ex,
-//                                                                  HttpHeaders headers, HttpStatus status, WebRequest request) {
-//        JsonError jsonError = new JsonError("Request validation Failed",
-//                ex.toString());
-//        return new ResponseEntity(jsonError, HttpStatus.BAD_REQUEST);
-//
-//    }
 
-    @ExceptionHandler ({Exception.class})
-    protected  ResponseEntity<Object> handleExceptionHandlerExceptionResolver(Exception ex) {
-        JsonError jsonError = new JsonError("error",
+    @Override
+    protected ResponseEntity<Object> handleHttpMessageNotReadable(HttpMessageNotReadableException ex,
+                                                                  HttpHeaders headers, HttpStatus status, WebRequest request) {
+        JsonError jsonError = new JsonError("Request validation Failed",
+                ex.toString());
+        return new ResponseEntity(jsonError, HttpStatus.BAD_REQUEST);
+
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleMissingServletRequestParameter(
+            MissingServletRequestParameterException ex, HttpHeaders headers,
+            HttpStatus status, WebRequest request) {
+        JsonError jsonError = new JsonError("Request validation Failed",
+                ex.toString());
+        return new ResponseEntity(jsonError, HttpStatus.BAD_REQUEST);
+
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+        JsonError jsonError = new JsonError("Request validation Failed",
                 ex.toString());
         return new ResponseEntity(jsonError, HttpStatus.BAD_REQUEST);
     }
 
 
+    @Override
+    protected ResponseEntity<Object> handleNoHandlerFoundException(NoHandlerFoundException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+        JsonError jsonError = new JsonError(ex.getRequestURL() + " not found",
+                ex.toString());
+        return new ResponseEntity(jsonError, HttpStatus.NOT_FOUND);
+    }
 
-
+    @ExceptionHandler({ MethodArgumentTypeMismatchException.class })
+    public ResponseEntity<Object> handleMethodArgumentTypeMismatch(
+            MethodArgumentTypeMismatchException ex, WebRequest request) {
+        JsonError jsonError = new JsonError(ex.getName() + " should be of type " + ex.getRequiredType().getName(),
+                ex.toString());
+        return new ResponseEntity(jsonError, HttpStatus.BAD_REQUEST);
+    }
 
 }
