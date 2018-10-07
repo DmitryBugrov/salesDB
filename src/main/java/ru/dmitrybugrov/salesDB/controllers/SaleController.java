448b050ddd865f98f4fcfe0308b213aec8a9d039
dmitrybugrov <dmitrybugrov@gmail.com>
2018-10-07 22:50:33 +0300

    add Sale controller

--- src/main/java/ru/dmitrybugrov/salesDB/controllers/SaleController.java
+++ src/main/java/ru/dmitrybugrov/salesDB/controllers/SaleController.java
@@ -1,34 +1,35 @@
 package ru.dmitrybugrov.salesDB.controllers;
 
+import lombok.extern.log4j.Log4j;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-import ru.dmitrybugrov.salesDB.model.InvoiceLine;
 import ru.dmitrybugrov.salesDB.model.JsonError;
-import ru.dmitrybugrov.salesDB.repositories.InvoiceLineRepository;
+import ru.dmitrybugrov.salesDB.model.Sale;
+import ru.dmitrybugrov.salesDB.repositories.SaleRepository;
 
 import javax.validation.Valid;
+import javax.xml.ws.Response;
 
 @RestController
-public class InvoiceLineController {
-    private static final Logger log = Logger.getLogger(InvoiceLineController.class);
+public class SaleController {
+    private static final Logger log = Logger.getLogger(SaleController.class);
 
-    private InvoiceLineRepository repo;
+    private SaleRepository repo;
 
     @Autowired
-    public InvoiceLineController(InvoiceLineRepository repo) {
+    public SaleController(SaleRepository repo) {
         this.repo = repo;
     }
 
-    public ResponseEntity<InvoiceLine> add(@Valid @RequestBody InvoiceLine invoice, BindingResult bindingResult) {
-        log.debug(invoice.toString());
+    public ResponseEntity<Sale> add (@Valid Sale sale, BindingResult bindingResult) {
+        log.debug(sale.toString());
         if (bindingResult.hasErrors()) return returnErrorBinding(bindingResult);
 
-        return new ResponseEntity(repo.save(invoice), HttpStatus.OK);
+        return new ResponseEntity<Sale>(repo.save(sale),HttpStatus.OK);
 
     }
 
